0c46ee6bf88a2a4dd425607489d2b4b88159187b
Adin Okta Triqadafi <triqadafi@gmail.com>
2020-10-19 18:10:49 +0700

    up

--- CorelLaserBot/FormMain.cs
+++ CorelLaserBot/FormMain.cs
@@ -100,6 +100,7 @@ namespace CorelLASERBot
 
         }
 
+
         private void DoSomething(CancellationToken token)
         {
             // Do something important.
@@ -152,7 +153,11 @@ namespace CorelLASERBot
                     {
                         //SetForegroundWindow(p[0].MainWindowHandle);
                         Thread.Sleep(1000);
-                        LeftMouseClick(Settings.Default.CURSOR_X, Settings.Default.CURSOR_Y);
+                        //LeftMouseClick(Settings.Default.CURSOR_X, Settings.Default.CURSOR_Y);
+                        Invoke((MethodInvoker)delegate ()
+                        {
+                            ClickOnPointTool.ClickOnPoint(this.Handle, new Point(Settings.Default.CURSOR_X, Settings.Default.CURSOR_Y));
+                        });
                         //SendKeys.SendWait("{ENTER}");
                         //var task = Task.Run(() => DoSomething(token), token);
                         //return;
@@ -437,6 +442,70 @@ namespace CorelLASERBot
         {
             Run_AutoClose();
         }
+    }
+
+
+    public class ClickOnPointTool
+    {
+
+        [DllImport("user32.dll")]
+        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+#pragma warning disable 649
+        internal struct INPUT
+        {
+            public UInt32 Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+        internal struct MOUSEINPUT
+        {
+            public Int32 X;
+            public Int32 Y;
+            public UInt32 MouseData;
+            public UInt32 Flags;
+            public UInt32 Time;
+            public IntPtr ExtraInfo;
+        }
+
+#pragma warning restore 649
+
+
+        public static void ClickOnPoint(IntPtr wndHandle, Point clientPoint)
+        {
+            var oldPos = Cursor.Position;
+
+            /// get screen coordinates
+            ClientToScreen(wndHandle, ref clientPoint);
+
+            /// set cursor on coords, and press mouse
+            Cursor.Position = new Point(clientPoint.X, clientPoint.Y);
+
+            var inputMouseDown = new INPUT();
+            inputMouseDown.Type = 0; /// input type mouse
+            inputMouseDown.Data.Mouse.Flags = 0x0002; /// left button down
+
+            var inputMouseUp = new INPUT();
+            inputMouseUp.Type = 0; /// input type mouse
+            inputMouseUp.Data.Mouse.Flags = 0x0004; /// left button up
+
+            var inputs = new INPUT[] { inputMouseDown, inputMouseUp };
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            /// return mouse 
+            Cursor.Position = oldPos;
+        }
+
     }
 }
 
