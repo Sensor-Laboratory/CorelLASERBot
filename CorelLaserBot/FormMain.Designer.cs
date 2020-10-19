7c80d60f5284c88d0db872dde76e6408fbdffdf9
Adin Okta Triqadafi <triqadafi@gmail.com>
2020-10-19 15:14:32 +0700

    add files

---
+++ CorelLaserBot/FormMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CorelLASERBot
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonCapture = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(103, 4);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.TabStop = false;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timerMain
+            // 
+            this.timerMain.Enabled = true;
+            this.timerMain.Interval = 3000;
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(12, 9);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(85, 13);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "Idle...";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelStatus.Click += new System.EventHandler(this.labelStatus_Click);
+            this.labelStatus.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // buttonCapture
+            // 
+            this.buttonCapture.Location = new System.Drawing.Point(184, 4);
+            this.buttonCapture.Name = "buttonCapture";
+            this.buttonCapture.Size = new System.Drawing.Size(27, 23);
+            this.buttonCapture.TabIndex = 4;
+            this.buttonCapture.TabStop = false;
+            this.buttonCapture.Text = "...";
+            this.buttonCapture.UseVisualStyleBackColor = true;
+            this.buttonCapture.Click += new System.EventHandler(this.buttonCapture_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 101);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(125, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(79, 101);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(215, 162);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonCapture);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonCapture;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ CorelLaserBot/FormMain.cs
@@ -0,0 +1,344 @@
+﻿using CorelLASERBot.Properties;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorelLASERBot
+{
+    public partial class FormMain : Form
+    {
+        // Create CancellationTokenSource.
+        CancellationTokenSource source;
+        // ... Get Token from source.
+        CancellationToken token;
+        bool TEMPLATE_FOUND = false;
+        bool AUTO_CLOSE = false;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (buttonStart.Tag.ToString() == "active")
+            {
+                buttonStart.Text = "Start!";
+                buttonStart.Tag = "";
+                source.Cancel();
+                BackColor = Color.White;
+                buttonCapture.Enabled = true;
+                // timerMain.Enabled = false;
+            }
+            else
+            {
+                if (!File.Exists("template.bmp"))
+                {
+                    TEMPLATE_FOUND = false;
+                }
+                else
+                {
+                    TEMPLATE_FOUND = true;
+                }
+
+                if (MessageBox.Show("Jalankan Program dengan Lokasi {X: " + Settings.Default.CURSOR_X + "; Y:" + Settings.Default.CURSOR_Y + ";}? \n\nDefault {X: " + Settings.Default.Properties["CURSOR_X"].DefaultValue + "; Y:" + Settings.Default.Properties["CURSOR_Y"].DefaultValue + ";}", Text, MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.OK)
+                {
+                    TopMost = true;
+                    BringToFront();
+                    buttonStart.Text = "Stop";
+                    buttonStart.Tag = "active";
+                    buttonCapture.Enabled = false;
+                    buttonStart.Enabled = false;
+                    source = new CancellationTokenSource();
+                    token = source.Token;
+                    var task = Task.Run(() => DoSomething(token), token);
+                    BackColor = Color.LawnGreen;
+                    await Task.Run(() => { Thread.Sleep(1000); });
+
+                    buttonStart.Enabled = true;
+                    // timerMain.Enabled = true;
+                }
+
+            }
+            labelStatus.Focus();
+        }
+
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            var x = MousePosition.X;
+            var y = MousePosition.Y;
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+            SetCursorPos(x, y);
+
+        }
+
+        private void DoSomething(CancellationToken token)
+        {
+            // Do something important.
+            while (true)
+            {
+                Invoke((MethodInvoker)delegate ()
+                {
+                    labelStatus.Text = "Running...";
+                });
+                // Wait a few moments.
+                Thread.Sleep(1000);
+
+
+                Bitmap screenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                Graphics g = Graphics.FromImage(screenCapture);
+
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                 Screen.PrimaryScreen.Bounds.Y,
+                                 0, 0,
+                                 screenCapture.Size,
+                                 CopyPixelOperation.SourceCopy);
+                var bmpSmall = (Bitmap)Resources.template;
+                if (TEMPLATE_FOUND)
+                {
+                    bmpSmall = (Bitmap)Image.FromFile("template.bmp");
+                }
+                Bitmap small = (Bitmap)bmpSmall.Clone();
+                Bitmap big = (Bitmap)screenCapture.Clone();
+                Invoke((MethodInvoker)delegate ()
+                {
+                    pictureBox1.Image = screenCapture;
+                    pictureBox2.Image = bmpSmall;
+                });
+
+                Invoke((MethodInvoker)delegate ()
+                {
+                    labelStatus.Text = "1!";
+                });
+
+                var test = false;
+                using (
+                    Bitmap
+                        _small = new Bitmap(small),
+                        _big = new Bitmap(big)
+)
+                {
+                    test = IsInCapture(_small, _big);
+
+
+                    //var test = false;
+                    Invoke((MethodInvoker)delegate ()
+                    {
+                        labelStatus.Text = "2!";
+                    });
+                    if (test)
+                    {
+
+                        Invoke((MethodInvoker)delegate ()
+                        {
+                            labelStatus.Text = "Found!";
+                        });
+                        string app = "CorelDRW";
+                        Process[] p = Process.GetProcessesByName(app);
+                        //Process[] p = Process.GetProcessesByName("Notepad");
+
+                        // Activate the first application we find with this name
+                        if (p.Count() > 0)
+                        {
+                            //SetForegroundWindow(p[0].MainWindowHandle);
+                            Thread.Sleep(1000);
+                            LeftMouseClick(Settings.Default.CURSOR_X, Settings.Default.CURSOR_Y);
+                            //SendKeys.SendWait("{ENTER}");
+                            //var task = Task.Run(() => DoSomething(token), token);
+                            //return;
+                        }
+                        else
+                        {
+                            Invoke((MethodInvoker)delegate ()
+                            {
+                                labelStatus.Text = "No " + app + "!";
+                            });
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    else
+                    {
+
+                        Invoke((MethodInvoker)delegate ()
+                        {
+                            labelStatus.Text = "Continue...";
+                        });
+                        Thread.Sleep(1000);
+
+                    }
+
+                    // See if we are canceled from our CancellationTokenSource.
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Method1 canceled");
+                        break;
+                    }
+                }
+            }
+            Invoke((MethodInvoker)delegate ()
+            {
+                labelStatus.Text = "Idle...";
+            });
+        }
+
+        private bool IsInCapture(Bitmap searchFor, Bitmap searchIn)
+        {
+            //for (int x = 0; x < searchIn.Width; x++)
+            //{
+            //    for (int y = 0; y < searchIn.Height; y++)
+            //    {
+            //        bool invalid = false;
+            //        int k = x, l = y;
+            //        for (int a = 0; a < searchFor.Width; a++)
+            //        {
+            //            l = y;
+            //            for (int b = 0; b < searchFor.Height; b++)
+            //            {
+            //                if (searchFor.GetPixel(a, b) != searchIn.GetPixel(k, l))
+            //                {
+            //                    invalid = true;
+            //                    break;
+            //                }
+            //                else
+            //                    l++;
+            //                if (token.IsCancellationRequested) break;
+            //            }
+            //            if (invalid)
+            //                break;
+            //            else
+            //                k++;
+            //            if (token.IsCancellationRequested) break;
+            //        }
+            //        if (!invalid)
+            //            return true;
+            //        if (token.IsCancellationRequested) break;
+            //    }
+            //    if (token.IsCancellationRequested) break;
+            //}
+
+            Thread.Sleep(5000);
+
+            return false;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            //var task = Task.Run(() => tracker());
+            buttonStart.Tag = "inactive";
+            Location = new Point(Convert.ToInt32(Screen.PrimaryScreen.Bounds.Width * 0.6), 0);
+            //Size = new Size(215, 30);
+            labelStatus.Focus();
+            Run_AutoClose();
+
+            Settings.Default.Reset();
+        }
+
+        private void tracker()
+        {
+            while (true)
+            {
+                Invoke((MethodInvoker)delegate ()
+                {
+                    //labelMousePosition.Text = "{X: " + MousePosition.X + "; Y:" + MousePosition.Y + ";}";
+                });
+                Thread.Sleep(500);
+            }
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void labelMousePosition_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void buttonCapture_Click(object sender, EventArgs e)
+        {
+            labelStatus.Focus();
+            if(MessageBox.Show("Posisikan cursor pada lokasi yang akan di click. Software akan menunggu selama 5 detik kemudian menyimpan posisinya.\n\nTekan \"OK\" untuk mengubah posisi\nTekan \"Cancel\" untuk kembali", "Cursor Location Capture", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.OK)
+            {
+                await Task.Run(() => { Thread.Sleep(5000); });
+                Settings.Default.CURSOR_X = MousePosition.X;
+                Settings.Default.CURSOR_Y = MousePosition.Y;
+                Settings.Default.Save();
+                MessageBox.Show("Lokasi {X: " + Settings.Default.CURSOR_X + "; Y:" + Settings.Default.CURSOR_Y + ";} berhasil disimpan!", "Cursor Location Capture", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (MessageBox.Show("Reset ke nilai default?", "Cursor Location Capture", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+
+                    Settings.Default.CURSOR_X = Convert.ToInt32(Settings.Default.Properties["CURSOR_X"].DefaultValue);
+                    Settings.Default.CURSOR_Y = Convert.ToInt32(Settings.Default.Properties["CURSOR_Y"].DefaultValue);
+                    Settings.Default.Save();
+                    MessageBox.Show("Lokasi {X: " + Settings.Default.CURSOR_X + "; Y:" + Settings.Default.CURSOR_Y + ";} berhasil direset ke default!", "Cursor Location Capture", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+        private void Run_AutoClose()
+        {
+
+            //Process[] p = Process.GetProcessesByName("CorelDRW");
+            Process[] p = Process.GetProcessesByName("notepad");
+
+            // Activate the first application we find with this name
+            if (p.Count() > 0)
+            {
+                AUTO_CLOSE = true;
+            }
+            else
+            {
+                if (AUTO_CLOSE)
+                    Application.Exit();
+            }
+        }
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            Run_AutoClose();
+        }
+    }
+}
+
