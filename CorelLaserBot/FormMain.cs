5d1ca8104dc7f7200ca6140539f9c793f57b26c1
Adin Okta Triqadafi <triqadafi@gmail.com>
2020-10-19 19:15:59 +0700

    up

--- CorelLaserBot/FormMain.cs
+++ CorelLaserBot/FormMain.cs
@@ -153,11 +153,7 @@ namespace CorelLASERBot
                     {
                         //SetForegroundWindow(p[0].MainWindowHandle);
                         Thread.Sleep(1000);
-                        //LeftMouseClick(Settings.Default.CURSOR_X, Settings.Default.CURSOR_Y);
-                        Invoke((MethodInvoker)delegate ()
-                        {
-                            ClickOnPointTool.ClickOnPoint(this.Handle, new Point(Settings.Default.CURSOR_X, Settings.Default.CURSOR_Y));
-                        });
+                        LeftMouseClick(Settings.Default.CURSOR_X, Settings.Default.CURSOR_Y);
                         //SendKeys.SendWait("{ENTER}");
                         //var task = Task.Run(() => DoSomething(token), token);
                         //return;
@@ -194,56 +190,6 @@ namespace CorelLASERBot
             {
                 labelStatus.Text = "Idle...";
             });
-        }
-
-        private bool IsInCapture(Bitmap searchFor, Bitmap searchIn)
-        {
-            int iterate = 0;
-            for (int x = 0; x < searchIn.Width; x++)
-            {
-                for (int y = 0; y < searchIn.Height; y++)
-                {
-                    bool invalid = false;
-                    int k = x, l = y;
-                    for (int a = 0; a < searchFor.Width; a++)
-                    {
-                        l = y;
-                        for (int b = 0; b < searchFor.Height; b++)
-                        {
-                            if (searchFor.GetPixel(a, b) != searchIn.GetPixel(k, l))
-                            {
-                                invalid = true;
-                                break;
-                            }
-                            else
-                                l++;
-                            iterate++;
-                            Invoke((MethodInvoker)delegate ()
-                            {
-                                labelStatus.Text = "I:" + iterate + "\n" +
-                                "X:" + searchIn.Width + " - " + x + "\n" +
-                                "Y:" + searchIn.Height + " - " + y + "\n" +
-                                "A:" + searchFor.Width + " - " + a + "\n" +
-                                "B:" + searchFor.Height + " - " + b + 
-                                "";
-                            });
-
-                            if (token.IsCancellationRequested) break;
-                        }
-                        if (invalid)
-                            break;
-                        else
-                            k++;
-                        if (token.IsCancellationRequested) break;
-                    }
-                    if (!invalid)
-                        return true;
-                    if (token.IsCancellationRequested) break;
-                }
-                if (token.IsCancellationRequested) break;
-            }
-
-            return false;
         }
 
         private Rectangle searchBitmap(Bitmap smallBmp, Bitmap bigBmp, double tolerance)
@@ -442,70 +388,8 @@ namespace CorelLASERBot
         {
             Run_AutoClose();
         }
-    }
-
-
-    public class ClickOnPointTool
-    {
-
-        [DllImport("user32.dll")]
-        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
-
-        [DllImport("user32.dll")]
-        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
-
-#pragma warning disable 649
-        internal struct INPUT
-        {
-            public UInt32 Type;
-            public MOUSEKEYBDHARDWAREINPUT Data;
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        internal struct MOUSEKEYBDHARDWAREINPUT
-        {
-            [FieldOffset(0)]
-            public MOUSEINPUT Mouse;
-        }
-
-        internal struct MOUSEINPUT
-        {
-            public Int32 X;
-            public Int32 Y;
-            public UInt32 MouseData;
-            public UInt32 Flags;
-            public UInt32 Time;
-            public IntPtr ExtraInfo;
-        }
-
-#pragma warning restore 649
-
-
-        public static void ClickOnPoint(IntPtr wndHandle, Point clientPoint)
-        {
-            var oldPos = Cursor.Position;
-
-            /// get screen coordinates
-            ClientToScreen(wndHandle, ref clientPoint);
-
-            /// set cursor on coords, and press mouse
-            Cursor.Position = new Point(clientPoint.X, clientPoint.Y);
-
-            var inputMouseDown = new INPUT();
-            inputMouseDown.Type = 0; /// input type mouse
-            inputMouseDown.Data.Mouse.Flags = 0x0002; /// left button down
-
-            var inputMouseUp = new INPUT();
-            inputMouseUp.Type = 0; /// input type mouse
-            inputMouseUp.Data.Mouse.Flags = 0x0004; /// left button up
-
-            var inputs = new INPUT[] { inputMouseDown, inputMouseUp };
-            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
-
-            /// return mouse 
-            Cursor.Position = oldPos;
-        }
 
     }
+
 }
 
