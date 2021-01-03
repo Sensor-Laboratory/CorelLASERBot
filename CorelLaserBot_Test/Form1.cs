c5a21df4a7920b52eeb7498cf9622a3c4d93a5e8
Adin Okta Triqadafi <triqadafi@gmail.com>
2021-01-03 09:41:01 +0700

    include test

---
+++ CorelLaserBot_Test/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorelLaserBot_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test", "Test", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+        }
+    }
+}
